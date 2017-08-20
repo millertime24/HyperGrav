db2f14a9c7b613652d02b245334e1a750ddd3609
Andrew <milldog8@hotmail.com>
2017-08-19 18:47:09 -0600

    one

--- HG/WeatherViewController.swift
+++ HG/WeatherViewController.swift
@@ -16,6 +16,7 @@ class WeatherViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -23,6 +24,7 @@ class WeatherViewController: UIViewController, UISearchBarDelegate {
     
     
     override func viewDidAppear(_ animated: Bool) {
+        activity.isHidden = true
         if Reachability.isConnectedToNetwork() == true
         {
             print("Connected")
@@ -44,8 +46,22 @@ class WeatherViewController: UIViewController, UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let searchText = searchBar.text {
+            activity.startAnimating()
+            activity.isHidden = false
             WeatherController.weatherBySearchCity(searchText) { (result) in
-                guard let weatherResult = result else { return }
+                guard let weatherResult = result else {
+                    let controller = UIAlertController(title: "No Internet Detected", message: "Connection Failure", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                    self.activity.stopAnimating()
+                    self.activity.isHidden = true
+                    controller.addAction(ok)
+                    controller.addAction(cancel)
+                    
+                    self.present(controller, animated: true, completion: nil)
+                    return
+                }
+            
                 DispatchQueue.main.async { () in
                     self.cityNameLabel.text = weatherResult.cityName
                     if let temperatureC = weatherResult.temperatureC {
